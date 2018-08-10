fbda7780f7a00fb99226a5ad908f6a1c06c26f27
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 13:05:17 -0700

    log to find data on find doctor

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -1,9 +1,9 @@
 export class FindDoctor {
   getDoctor(response) {
     let body = JSON.parse(response);
-    let doctors = [];
-    console.log(body)
-    // body.drinks.forEach(function (drink) {
+    let doctorsArray = [];
+    console.log(body.data)
+    // body.data.forEach(function (drink) {
     //   let foundDrink = new Drink(drink.strDrink, drink.strDrinkThumb, drink.idDrink);
     //   drinks.push(foundDrink);
     // });
