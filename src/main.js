b1e68ce8f14e1c0ff2c0ef36e81008ba9d0a4d28
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 16:19:20 -0700

    add click function for doctors name and change output to a class

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { FindDoctor } from './findDoctor.js';
 
 $(document).ready(function(){
   $('#checkSymptom').click(function(){
-    $('#availableDoctors').text('')
+    $('.availableDoctors').text('')
     let symptom = $('#userSymptom').val();
     let findDoctorService = new FindDoctorService();
     let promise = findDoctorService.getDoctorBySymptom(symptom);
@@ -18,7 +18,39 @@ $(document).ready(function(){
       
         foundDoctors.forEach(doctor => {
           console.log(doctor)
-          $('#availableDoctors').append(
+          $('.availableDoctors').append(
+            `<div class="card">
+              <div class="row">
+                <div class="doctorImageContainer col-md-2">
+                  <img class="doctorImage" src=${doctor.picture}>
+                </div>
+                <div class="doctorInformation col">
+                  <p>${doctor.firstName} ${doctor.lastName}</p>
+                  <p>Address: ${doctor.address}</p>
+                  <p>Phone Number: ${doctor.phoneNumber} ${doctor.website}</p>
+                  <p>Accepting Patients: ${doctor.acceptingPatients}</p>
+                  <p>${doctor.bio}</p>
+                </div>
+              </div>
+              
+            </div>`
+        )
+      })
+    })
+  });
+  $('#findDoctor').click(function(){
+    $('.availableDoctors').text('')
+    let doctorName = $('#userDoctor').val();
+    let findDoctorService = new FindDoctorService();
+    let promise2 = findDoctorService.getDoctorByName(doctorName);
+    
+    promise2.then(function(response){
+      let findDoctors = new FindDoctor();
+      let foundDoctors = findDoctors.getDoctor(response)
+      
+        foundDoctors.forEach(doctor => {
+          console.log(doctor)
+          $('.availableDoctors').append(
             `<div class="card">
               <div class="row">
                 <div class="doctorImageContainer col-md-2">
