de3605929c532095aaed7ca7a54a755518e8be4c
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 15:41:00 -0700

    add conditional if website does not exist

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -7,6 +7,7 @@ export class FindDoctor {
     body.data.forEach(function (doctor) {
       let acceptingNewPatients = ""
       let street2 = ''
+      let website = ''
       if (doctor.practices[0].accepts_new_patients == true) {
         acceptingNewPatients = "Yes"
       } else {
@@ -16,9 +17,13 @@ export class FindDoctor {
         street2 = ''
       } else{
         street2 = doctor.practices[0].visit_address.street2 + ","
-        
       }
-      let doctors = new Doctor(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.image_url, doctor.profile.gender, doctor.profile.bio, `${doctor.practices[0].visit_address.street}, ` + street2 + ` ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}`, doctor.practices[0].phones[0].number, doctor.practices[0].website, acceptingNewPatients)
+      if (doctor.practices[0].website == null) {
+        website = ''
+      } else{
+        website = doctor.practices[0].website
+      }
+      let doctors = new Doctor(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.image_url, doctor.profile.gender, doctor.profile.bio, `${doctor.practices[0].visit_address.street}, ` + street2 + ` ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}`, doctor.practices[0].phones[0].number, acceptingNewPatients, website)
       console.log(street2)
       
       doctorsArray.push(doctors);
