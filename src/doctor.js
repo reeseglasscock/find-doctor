32eda34d51385f0e098c4abec415cab9d74a238f
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 13:12:48 -0700

    create doctor class to create new objects from API call

---
+++ src/doctor.js
@@ -0,0 +1,9 @@
+export class Doctor {
+  constructor(firstName, lastName, picture, gender, bio){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.picture = picture
+    this.gender = gender
+    this.bio = bio
+  }
+}
