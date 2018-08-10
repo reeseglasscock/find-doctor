ccc92411095b6a3af766da1b3e8037a9bac2da90
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 11:09:29 -0700

    Add in the find doctor class to parse the find doctor service class

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -1,17 +1,11 @@
-export class FindDoctorService {
-  getDoctorBySymptom(symptom) {
-    return new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=705b26eb924e6457d986841e5241ef17&query=acne`;
-      request.onload = () => {
-        if (request.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+export class FindDoctor {
+  getDoctor(response) {
+    let body = JSON.parse(response);
+    let doctors = [];
+    console.log(body)
+    // body.drinks.forEach(function (drink) {
+    //   let foundDrink = new Drink(drink.strDrink, drink.strDrinkThumb, drink.idDrink);
+    //   drinks.push(foundDrink);
+    // });
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { FindDoctorService } from './findDoctorService.js';
+import { FindDoctor } from './findDoctor.js';
 
 $(document).ready(function(){
   $('#checkSymptom').click(function(){
     let symptom = $('#userSymptom').val();
+    let findDoctorService = new FindDoctorService();
+    let findDoctor = new FindDoctor();
+    console.log(findDoctor);
+    
   });
 });
