dc5852c43e49b9ca96aebfb880be83786c59e756
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 10:28:50 -0700

    Minor updates for indentations

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
-    $('#checkSymptom').click(function(){
-        let symptom = $('#userSymptom').val()
-    })
-})
+  $('#checkSymptom').click(function(){
+    let symptom = $('#userSymptom').val();
+  });
+});
