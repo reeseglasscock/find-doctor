47bc16b396396cf5555529e23da6bce9384418c0
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 10:10:21 -0700

    add in document ready that collects user response on click

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function(){
+    $('#checkSymptom').click(function(){
+        let symptom = $('#userSymptom').val()
+    })
+})
