05c74553bd94ca950b02747220eda7b6c88207ce
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 16:08:58 -0700

    add plugin for dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 
 module.exports = {
@@ -21,7 +22,8 @@ module.exports = {
       title: 'Doctor Finder',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
