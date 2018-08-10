67850fd8f67ac418d91911868fdd1d0ea0c13777
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 16:14:38 -0700

    add method to get doctor by name

--- src/findDoctorService.js
+++ src/findDoctorService.js
@@ -14,4 +14,19 @@ export class FindDoctorService {
       request.send();
     });
   }
+  getDoctorByName(name) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = () => {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
