7dffbdad6b658e2734d58df38a2709a7af0cfa99
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 15:29:36 -0700

    add in conditional for accepting new patients to be either yes or no

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -5,7 +5,14 @@ export class FindDoctor {
     let body = JSON.parse(response);
     let doctorsArray = [];
     body.data.forEach(function (doctor) {
-      let doctors = new Doctor(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.image_url, doctor.profile.gender, doctor.profile.bio, `${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.street2}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}`, doctor.practices[0].phones[0].number, doctor.practices[0].website, doctor.practices[0].accepts_new_patients);
+      let acceptingNewPatients = ""
+      if (doctor.practices[0].accepts_new_patients == true) {
+        acceptingNewPatients = "Yes"
+      } else {
+        acceptingNewPatients = "No"
+      }
+      let doctors = new Doctor(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.image_url, doctor.profile.gender, doctor.profile.bio, `${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.street2}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}`, doctor.practices[0].phones[0].number, doctor.practices[0].website, acceptingNewPatients)
+      
       doctorsArray.push(doctors);
     });
     return doctorsArray;
