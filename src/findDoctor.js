b2a31f9b0d561190eb65e99740f419fe3d3fa38f
reeseglasscock <reesegpdx@gmail.com>
2018-08-10 13:24:28 -0700

    will create new object for each doctor with name, bio, image, and gender

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -5,7 +5,7 @@ export class FindDoctor {
     let body = JSON.parse(response);
     let doctorsArray = [];
     body.data.forEach(function (doctor) {
-      let doctors = new Doctor(doctor.profile);
+      let doctors = new Doctor(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.image_url, doctor.profile.gender, doctor.profile.bio);
       doctorsArray.push(doctors);
     });
     return doctorsArray;
